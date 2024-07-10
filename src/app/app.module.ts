846b254c503922d399394c450edc3464ee38d515
Claude Ishimwe <claude.ishimwe@amalitechtraining.org>
2024-07-10 13:32:53 +0200

    add steppers and nav bar on phone screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StepsComponent } from './components/steps/steps.component';
+import { NavButtonsComponent } from './components/nav-buttons/nav-buttons.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StepsComponent,
+    NavButtonsComponent
   ],
   imports: [
     BrowserModule,
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "sky-blue": "#BEE2FD",
+        Denim: "hsl(213, 96%, 18%)",
+        Puplish: "hsl(243, 100%, 62%)",
+        "Light-blue": "hsl(206, 94%, 87%)",
+        "Red-errors": "hsl(354, 84%, 57%)",
+        "Cool-gray": "hsl(231, 11%, 63%)",
+        "Light-gray": "hsl(229, 24%, 87%)",
+        Magnolia: "hsl(217, 100%, 97%)",
+        Alabaster: "hsl(231, 100%, 99%)",
+        white: "hsl(0, 0%, 100%)",
+      },
+    },
   },
   plugins: [],
 };
